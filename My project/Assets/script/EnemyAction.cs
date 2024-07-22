727b954747ba4b882de1941e1654b572b61d6fcb
18310036698 <2351267117@qq.com>
2024-07-22 22:51:28 +0800

    有很多bug
    索敌和攻击逻辑有问题

---
+++ My project/Assets/script/EnemyAction.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAction : MonoBehaviour
+{
+    float Startpos;
+    public int limitation;
+    public float speed;
+    public int[] Dir = new int[2];
+    public int i = 0, j = 0;
+    public int RandomDir;
+    public float startpos, dis, Dis, Position;
+    bool Spot_Player;
+    public LayerMask player;
+    public Transform Visibility;
+    public GameObject player_character;
+    public float Coefficient;
+    public bool spot;
+    public Rigidbody2D rbe;
+    public float jumpforce;
+    public int test_movement;
+    public GameObject player_position, enemy_position;
+    public float x;
+    public int ecurrentHealth;
+    public int emaxHealth;
+    PlayerAction con;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Startpos = transform.position.x;
+        rbe = GetComponent<Rigidbody2D>();
+        ecurrentHealth = emaxHealth;
+        con = GetComponent<PlayerAction>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Position = transform.position.x;
+        Dis = Startpos - Position;
+        if (Dis <= 0)
+        {
+            dis = -1 * Dis;
+        }
+        else
+        {
+            dis = Dis;
+        }
+        if (ecurrentHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move() 
+    {
+        Spot_Player = Physics2D.OverlapCircle(Visibility.position, 5f, player);
+        if (Spot_Player)
+        {
+            SpotPlayer();
+            spot = true;
+            test_movement = 1;
+        }
+        else
+        {
+            RandomMove();
+            spot = false;
+            test_movement = 2;
+        }
+    }
+    void RandomMove()
+    {
+        if (i == 75)
+        {
+            RandomDir = Random.Range(0, 2);
+            i = 0;
+            if (RandomDir == 0)
+            {
+                speed = speed * -1;
+            }
+        }
+        if (dis > limitation && !spot)
+        {
+            if (speed > 0 && Dis < 0)
+            {
+                speed = speed * -1;
+            }
+            if (speed < 0 && Dis > 0)
+            {
+                speed = speed * -1;
+            }
+            i = 0;
+        }
+        transform.Translate(speed, 0, 0);
+        i++;
+    }
+    void SpotPlayer()
+    {
+        Vector3 Enemy_moving = enemy_position.transform.position - player_position.transform.position;
+        if (Enemy_moving.x < 0)
+        {
+            x = 1 / (Enemy_moving.x * -1);
+        }
+        else
+        {
+            x = 1 / (Enemy_moving.x);
+        }
+        transform.Translate(Enemy_moving.x * x * Coefficient, 0, 0);
+        if (enemy_position.transform.position.y < player_position.transform.position.y)
+        {
+            if (j >= 100)
+            {
+                rbe.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+                j = 0;
+            }
+        }
+        j++;
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayerAction player = other.gameObject.GetComponent<PlayerAction>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("hitbox"))
+        {
+            eChangeHealth(-1);
+        }
+    }
+
+    public void eChangeHealth(int amount)
+    {
+        ecurrentHealth = Mathf.Clamp(ecurrentHealth + amount, 0, emaxHealth);
+        Debug.Log("Enemy" + ecurrentHealth + "/" + emaxHealth);
+    }
+
+}
--- My project/Assets/script/Playeraction.cs
+++ My project/Assets/script/Playeraction.cs
@@ -2,29 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playeraction : MonoBehaviour
+public class PlayerAction : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
-    public Transform GroundCheck;
-    public bool isground;
+    public Transform groundCheck;
+    [SerializeField]private bool isGrounded;
     public LayerMask env;
-    public int jumpcount;
+    public int jumpCount;
     public float gravity;
-    public float fallgravity;
-    public float maxjumptime;
-    public float jumptime;
-    public float jumpforce;
+    public float fallGravity;
+    public float maxJumptime;
+    public float jumpTime;
+    public float jumpForce;
+    public float speedx;
+    public bool face=true;
+    public float atkTime=0;
+    public float duration;
+    public float coolDown;
+    public GameObject atkArea1, atkArea2;
+    public bool isInvincible, isAtkable, atked;
+    public int currentHealth;
+    public int maxHealth;
+    public float timeInvincible = 2.0f, timeAtkable = 1.0f;
+    float invincibleTimer;
+    public int health { get { return currentHealth; } }
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        atkArea1.SetActive(false);
+        atkArea2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        jump();
+        Jump();
+        Attack();
+        Invincible();
     }
     void FixedUpdate()
     {
@@ -32,41 +49,41 @@ public class Playeraction : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         //实现地面检测
-        isground = Physics2D.OverlapCircle(GroundCheck.position, 0.1f, env);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, env);
     }
-    void jump()
+    void Jump()
     {
         if (Input.GetButton("Jump"))
         {
-            jumptime += Time.deltaTime;
+            jumpTime += Time.deltaTime;
         }
-        if (Input.GetButtonDown("Jump") && jumpcount >= 0)
+        if (Input.GetButtonDown("Jump") && jumpCount >= 0)
         {
-            if (isground)
+            if (isGrounded)
             {
-                jumpcount = 2;
+                jumpCount = 2;
             }
         }
-        if (Input.GetButtonDown("Jump") || isground)
+        if (Input.GetButtonDown("Jump") || isGrounded)
         {
             gravity = 5f;
         }
-        if (Input.GetButtonDown("Jump") && jumpcount > 0)
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
         {
-            if (jumpcount < 2 && jumpcount > 0)
+            if (jumpCount < 2 && jumpCount > 0)
             {
                 rb.velocity = new Vector2(rb.velocity.x, 0);
             }
             rb.gravityScale = gravity;
-            rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
-            jumpcount--;
-            jumptime = 0f;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount--;
+            jumpTime = 0f;
         }
         if (Input.GetButtonUp("Jump"))
         {
-            if (jumptime < maxjumptime)
+            if (jumpTime < maxJumptime)
             {
-                gravity = gravity * 1.5f / (0.5f + jumptime);
+                gravity = gravity * 1.5f / (0.5f + jumpTime);
             }
         }
         if (rb.velocity.y > 0)
@@ -75,7 +92,72 @@ public class Playeraction : MonoBehaviour
         }
         else
         {
-            rb.gravityScale = fallgravity;
+            rb.gravityScale = fallGravity;
+        }
+    }
+    bool Face()
+    {
+        speedx = rb.velocity.x;
+        if (speedx > 0)
+        {
+            face = true;
+        }
+        else if (speedx < 0)
+        {
+            face = false;
+        }
+        return face;
+    }
+    void Attack()
+    {
+        bool atkface = Face();
+        if (Input.GetButton("Fire1") && atkTime == 0)
+        {
+            //激活判定区域
+            if (atkface)
+            {
+                //激活右
+                atkArea2.SetActive(true);
+            }
+            else
+            {
+                //激活左
+                atkArea1.SetActive(true);
+            }
+            atkTime = Time.time;
+        }
+        if (Time.time-atkTime > duration)
+        {
+            //de激活判定区域
+            atkArea1.SetActive(false);
+            atkArea2.SetActive(false);
+        }
+        if (Time.time - atkTime > coolDown)
+        {
+            atkTime = 0;
+        }
+    }
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log("Player:" + currentHealth + "/" + maxHealth);
+    }
+    void Invincible()
+    {
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
         }
     }
 }
